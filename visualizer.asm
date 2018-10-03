a6ff25a37f9a47170b217c95e479b11ea00933cd
FIX94 <fix94.1@gmail.com>
2018-10-03 19:04:15 +0200

    added play/stop feature, titles of tracks, sorted tracks close to official soundtrack

--- visualizer.asm
+++ visualizer.asm
@@ -3,6 +3,7 @@
 ; This software may be modified and distributed under the terms
 ; of the MIT license.  See the LICENSE file for details.
 
+;re-use character map game left in memory
 include "charmap.asm"
 
 ;shared high variables
@@ -18,11 +19,13 @@ hGameRedrawBG EQU $FFD0
 hGameTileAnimation EQU $FFD7
 
 init:
+	;disable interrupts for
+	;game cleanup
 	di
 	;disable game oam dma
 	ld a, $C9 ;ret
 	ldh [hGameOAMDMAFunc], a
-	ld a, $0
+	xor a
 	;disable game background (re)draw
 	ldh [hGameAutoBGCopy], a
 	ldh [hGameVBlankCopy1Addr], a
@@ -41,16 +44,16 @@ init:
 	ld [gameDisableWindowScroll], a
 	;clear screen and draw header
 	call waitVBlank
-	ld a, $0
+	xor a
 	ldh [$FF40], a
 	call clearScreen
 	call drawHeader
 	;draw black bars
 	ld hl, $9940
 	call drawVolbar
-	ld hl, $9980
+	ld l, $80
 	call drawVolbar
-	ld hl, $99C0
+	ld l, $C0
 	call drawVolbar
 	ld hl, $9A00
 	call drawVolbar
@@ -64,22 +67,23 @@ bartileloop:
 	jr nz, bartileloop
 	;draw channel names
 	ld bc, $9921
-	ld hl, sq1msg
+	ld d, 8
+	ld hl, chanmsg
 	call drawLine
 	ld c, $61
-	ld hl, sq2msg
+	ld d, 8
 	call drawLine
 	ld c, $A1
-	ld hl, noisemsg
+	ld d, 5
 	call drawLine
 	ld c, $E1
-	ld hl, wavemsg
+	ld d, 4
 	call drawLine
 	;turn on screen
 	ld a, $E1
 	ldh [$FF40], a
 	;cover screen with window
-	ld a, $00
+	xor a
 	ldh [$FF4A], a
 	;scroll window out of the way for now
 	ld a, $A8
@@ -92,12 +96,10 @@ bartileloop:
 	ldh [$FF41], a
 	ld a, 3
 	ldh [$FFFF], a
-	ld a, 0
-	ldh [$FF0F], a
-	;call music function
-	ei
-	ld a, [songvar]
-	call playsong
+	;call music function,
+	;enables interrupts for us
+	xor a
+	call playSong
 	;draw volume loop
 tloop:
 	halt
@@ -131,9 +133,8 @@ prepframe:
 	call gameUpdateControls
 	ldh a, [hGameButtonsPressed]
 	ld b, a
-	and $30
+	and $33
 	jr z, newvol
-	ld a, b
 	call switchSong
 	jr tloop
 newvol:
@@ -212,25 +213,38 @@ dowindowscroll:
 	jp tloop
 
 switchSong:
-	and $10
-	jr nz, goright
-goleft:
+	;check b press
+	bit 1, b
+	jr z, notb
+	;stop play
+	ld a, $80
+	jr skipsaveandplay
+notb:
+	;used in a, left and right
 	ld a, [songvar]
+	;check a press
+	bit 0, b
+	jr z, checkleft
+	;(re)start play
+	jr skipsaveandplay
+checkleft:
+	bit 4, b
+	jr z, goleft
+goright: ;remaining bit is right
+	inc a
+	cp $32
+	jr nz, savesongandplay
+	xor a ;loop back to first
+	jr savesongandplay
+goleft:
 	dec a
 	cp $ff
 	jr nz, savesongandplay
-	ld a, $30
-	jr savesongandplay
-goright:
-	ld a, [songvar]
-	inc a
-	cp $31
-	jr nz, savesongandplay
-	ld a, 0
-	jr savesongandplay
+	ld a, $31 ;last song in list
 savesongandplay:
 	ld [songvar], a
-	call playsong
+skipsaveandplay:
+	call playSong
 	ret
 
 fillVol:
@@ -283,22 +297,45 @@ clrloop:
 
 drawLine:
 	ld a, [hl+]
-	cp 0
-	ret z
 	ld [bc], a
 	inc c
+	dec d
+	ret z
 	jr drawLine
 
 drawHeader:
 	ld bc, $9821
+	ld d, 16
 	ld hl, hdrmsg
 	call drawLine
 	ld c, $41
+	ld d, 13
+	call drawLine
+	ld c, $81
+	ld d, 16
+	call drawLine
+	ld c, $A1
+	ld d, 16
+	call drawLine
+	ret
+
+drawSongName:
+	sla c
+	sla c
+	sla c
+	ld b, 0
+	jr nc, songnameadd
+	inc b
+songnameadd:
+	ld hl, songnames
+	add hl, bc
+	ld bc, $98E3
+	ld d, 8
 	call drawLine
 	ret
 
 drawVolbar:
-	ld a, 0
+	xor a
 	ld b, $14
 volloop:
 	ld [hl+], a
@@ -306,51 +343,89 @@ volloop:
 	jr nz, volloop
 	ret
 
-dotlut:
-	db $0A, $14, $1E, $28, $32, $3C, $46, $50, $5A, $64, $6E, $78, $82, $8C, $96, $A0
-
-volvar:
-	db $00, $00
-
-playsong:
+playSong:
+	;disable interrupts for vblank
+	di
+	;save song value
+	ld c, a
+	push bc
+	;load vals for stop
+	ld a, $FF
+	ld c, $1F
+	;first stop current song
+	call gamePlaySong
+	;wait for vblank so we can write to vram
+	call waitVBlank
+	;draw play/stop tile
+	pop bc
+	bit 7, c
+	jr z, playtile
+	;stop requested
+	xor a ;black box for stop
+	ld [$98E1], a
+	jr playsongend
+playtile: ;keep playing
+	ld a, $ED ;arrow for play
+	ld [$98E1], a
+	;draw song name
+	push bc
+	call drawSongName
+	pop bc
+	;then load next from LUT
 	ld hl, songlut
 	ld b, 0
-	ld c, a
 	sla c
 	add hl, bc
 	ld a, [hl+]
 	ld c, a
 	ld a, [hl]
 	call gamePlaySong
+playsongend:
+	;clear interrupt flags
+	xor a
+	ldh [$FF0F], a
+	;re-enable interrupts
+	ei
 	ret
 
-songlut:
-	;bank 02 songs
-	db $02, $BA, $02, $BD, $02, $C0, $02, $C3, $02, $C7, $02, $CA, $02, $CD, $02, $D0, $02, $D4, $02, $D8
-	db $02, $DB, $02, $DE, $02, $E1, $02, $E5, $02, $E8, $02, $EB, $02, $EF, $02, $F3, $02, $F7, $02, $FB
-	;bank 08 songs
-	db $08, $EA, $08, $ED, $08, $F0, $08, $F3, $08, $F6, $08, $F9, $08, $FC
-	;bank 1F songs
-	db $1F, $C3, $1F, $C7, $1F, $CA, $1F, $CD, $1F, $D0, $1F, $D2, $1F, $D6, $1F, $D9, $1F, $DC, $1F, $DF
-	db $1F, $E3, $1F, $E7, $1F, $EB, $1F, $EF, $1F, $F2, $1F, $F5, $1F, $F8, $1F, $FB
-	;bank 20 songs
-	db $20, $99, $20, $9C, $20, $9F, $20, $A3
+dotlut:
+	;window positions of the volume bar
+	db $07, $12, $1C, $27, $32, $3C, $47, $52, $5C, $67, $72, $7C, $87, $92, $9C, $A7
 
-songvar:
-	db $00
+songlut:
+	;bank 02, 08, 1F songs ordered to match official soundtrack, ended by bank 20 songs, yellow exclusives
+	db $1F, $DC, $1F, $C3, $02, $BA, $02, $DB, $1F, $CD, $02, $DE, $02, $EB, $08, $F0, $08, $F9, $02, $C3
+	db $02, $BD, $02, $E8, $1F, $E3, $02, $E1, $1F, $F8, $08, $ED, $08, $F6, $1F, $E7, $02, $F3, $02, $C7
+	db $02, $C0, $02, $EF, $1F, $D0, $02, $D0, $02, $D8, $02, $F7, $02, $B8, $1F, $FB, $08, $EA, $08, $FC
+	db $1F, $D2, $02, $D4, $1F, $EF, $02, $CA, $1F, $D9, $1F, $F5, $1F, $DF, $1F, $F2, $1F, $D6, $02, $CD
+	db $1F, $EB, $02, $E5, $02, $FB, $08, $F3, $1F, $CA, $1F, $C7, $20, $99, $20, $9C, $20, $9F, $20, $A3
+
+songnames:
+	;shortened track names sorted to match LUT above, all 8 characters in length to fit into ram
+	db "Opening ", "Title   ", "Pallet  ", "Prof.Oak", "Oaks Lab", "Rival   ", "Routes 1", "Battle 1", "Victory1", "Pewter  "
+	db "P.Center", "Healed  ", "V.Forest", "Guide   ", "Appear 1", "Battle 2", "Victory2", "Caves   ", "Routes 2", "Cerulean"
+	db "Pkm.Gym ", "Routes 3", "Jig.Puff", "Vermill.", "S.S.Anne", "Routes 4", "Pk.Flute", "Appear 2", "Battle 3", "Victory3"
+	db "Cycling ", "Lavender", "Pk.Tower", "Celadon ", "Game Co.", "Appear 3", "Hideout ", "SilphCo.", "Surfing ", "Cinnabar"
+	db "Mansion ", "SafariZ.", "Routes 5", "Battle 4", "H.OfFame", "Ending  ", "SurfPika", "Appear 4", "Unused  ", "Printer "
 
 hdrmsg:
-	db "Sound Visualizer ", $00
-	db "v0.1 by FIX94", $00
+	;header on top of image
+	db "Sound Visualizer"
+	db "v0.2 by FIX94"
+	db "A: Play, B: Stop"
+	db "L: Prev, R: Next"
 
-sq1msg:
-	db "Square 1", $00
+chanmsg:
+	;audio channel names
+	db "Square 1"
+	db "Square 2"
+	db "Noise"
+	db "Wave"
 
-sq2msg:
-	db "Square 2", $00
+volvar:
+	;volumes for current frame
+	db $00, $00
 
-noisemsg:
-	db "Noise", $00
-
-wavemsg:
-	db "Wave", $00
+songvar:
+	;song currently selected
+	db $00
